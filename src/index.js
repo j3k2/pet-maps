8f4225a1c7e8c76ebaec48ecc09c3a057db0f2ea
John Kim <jksfo@pm.me>
2018-08-29 22:54:41 -0700

    remove sliding panel

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import thunk from 'redux-thunk';
 import Geocode from "react-geocode";
 import SearchInput from './SearchInput';
 import SlidingPanel from './SlidingPanel';
+import SheltersList from './SheltersList';
+import MapWrapper from './MapWrapper';
 import { Icon } from 'semantic-ui-react';
 import WebFont from 'webfontloader';
 import './main.css';
@@ -31,30 +33,42 @@ class App extends Component {
     return (
       <Provider store={createStoreWithMiddleware(reducers)}>
         <div>
-          <div style={{ position: 'fixed', height: 70, zIndex: 999, width: '100%', fontSize: 18, background: '#f2711c', color: 'white' }}>
+          <div style={{ position: 'fixed', height: 70, zIndex: 999, width: '100%', minWidth: 840, fontSize: 18, background: '#f2711c', color: 'white' }}>
             <div style={{ padding: 10, paddingLeft: 20 }}>
-              <Icon name="paw" />
-              <span style={{ fontFamily: 'Luckiest Guy', marginLeft: 5, marginRight: 30 }}>PET MAPS</span>
+              <div style={{ display: 'inline-block', whiteSpace: 'nowrap' }}>
+                <Icon name="paw" />
+                <span style={{ fontFamily: 'Luckiest Guy', marginLeft: 5, marginRight: 30 }}>PET MAPS</span>
+              </div>
               <SearchInput
-                showPanel={() => {
-                  this.setState({ panelVisible: true });
-                }}
+              // showPanel={() => {
+              //   this.setState({ panelVisible: true });
+              // }}
               />
-              <span
+              {/* <span
                 style={{ float: 'right', paddingTop: 10, paddingRight: 15, cursor: 'pointer' }}
                 onClick={() => {
                   this.setState({ panelVisible: !this.state.panelVisible })
                 }}>
-                {/* <span style={{ marginRight: 10 }}>
-                  Toggle Map
-                </span> */}
                 <Icon name="map"></Icon>
                 <Icon name={this.state.panelVisible ? 'toggle on' : 'toggle off'}></Icon>
-                </span>
+              </span> */}
             </div>
-            <SlidingPanel visible={this.state.panelVisible} center={this.state.center} />
+            {/* <SlidingPanel visible={this.state.panelVisible} center={this.state.center} /> */}
           </div>
-          <div style={{ display: 'inline-block', marginTop: this.state.panelVisible ? 530 : 70, transition: 'all 1s' }}>
+          <div style={{ display: 'inline-block', marginTop: 70 }}>
+            <div style={{
+              width: '100%',
+              minWidth: 840,
+              padding: 20,
+              margin: '0 auto'
+            }}>
+              <div style={{ display: 'inline-block' }}>
+                <MapWrapper />
+              </div>
+              <div style={{ display: 'inline-block', width: 400 }}>
+                <SheltersList />
+              </div>
+            </div>
             <PetsView />
           </div>
         </div >
