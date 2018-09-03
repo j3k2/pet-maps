61c1c2d7b617d4a1bdafde8941a0921f2fdfa6b3
John Kim <jksfo@pm.me>
2018-09-02 20:29:39 -0700

    ImageViewer fixes

--- src/ImageViewer.js
+++ src/ImageViewer.js
@@ -18,28 +18,30 @@ class ImageViewer extends Component {
                         cursor: 'pointer'
                     }}
                     onClick={() => {
-                        this.setState({ modalOpen: true });
+                        if(this.props.images && this.props.images.length) {
+                            this.setState({ modalOpen: true });
+                        }
                     }}
                     width={228}
                     height={228}
                     src={this.props.images ? this.props.images[this.state.imageIndex].$t : ''}></Image>
-                <span style={{ textAlign: 'center' }}>
-                    <Icon
-                        onClick={() => {
-                            this.setState({ imageIndex: this.props.images && this.state.imageIndex === 0 ? this.props.images.length - 1 : this.state.imageIndex - 1 })
-                        }}
-                        style={{ cursor: 'pointer', float: 'left', position: 'relative', left: 10, bottom: 10 }}
-                        name="arrow alternate circle left"></Icon>
+                {(!this.props.images || this.props.images.length === 0) && <span style={{  position: 'relative', bottom: 10, textAlign: 'center' }}>No photos</span>}
+                {this.props.images && this.props.images.length === 1 && <span style={{ position: 'relative', bottom: 10, textAlign: 'center' }}>{this.state.imageIndex + 1}/{this.props.images.length}</span>}
+                {this.props.images && this.props.images.length > 1 && <span style={{ textAlign: 'center' }}><Icon
+                    onClick={() => {
+                        this.setState({ imageIndex: this.state.imageIndex === 0 ? this.props.images.length - 1 : this.state.imageIndex - 1 })
+                    }}
+                    style={{ cursor: 'pointer', float: 'left', position: 'relative', left: 10, bottom: 10 }}
+                    name="arrow alternate circle left"></Icon>
                     <span style={{ position: 'relative', bottom: 10 }}>
-                        {this.state.imageIndex + 1}/{this.props.images && this.props.images.length}
+                        {this.state.imageIndex + 1}/{this.props.images.length}
                     </span>
                     <Icon
                         onClick={() => {
-                            this.setState({ imageIndex: this.props.images && this.state.imageIndex === this.props.images.length - 1 ? 0 : this.state.imageIndex + 1 })
+                            this.setState({ imageIndex: this.state.imageIndex === this.props.images.length - 1 ? 0 : this.state.imageIndex + 1 })
                         }}
                         style={{ cursor: 'pointer', float: 'right', position: 'relative', right: 10, bottom: 10 }}
-                        name="arrow alternate circle right"></Icon>
-                </span>
+                        name="arrow alternate circle right"></Icon></span>}
                 <Modal style={{ width: 'auto', textAlign: 'center' }} open={this.state.modalOpen} onClose={() => {
                     this.setState({ modalOpen: false });
                 }}>
